fac9b4e39fd14420648e2775b8693127eacbf973
Lozi97 <ahmed.elozy97@gmail.com>
2018-02-27 02:38:14 +0200

    login & signup updated

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -42,9 +42,7 @@ const routes: Routes = [
       },
     ],
   },
-  {
-
-    path: 'dashboard',
+  {    path: 'dashboard',
     loadChildren: './dashboard/dashboard.module#DashboardModule'
   },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
